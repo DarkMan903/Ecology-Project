aa7a431e80d4d89879297e12209f0e8f09bcc071
DarkMan903 <156571062+DarkMan903@users.noreply.github.com>
2024-08-10 19:47:49 +1000

    main.py

--- Ecology_Project/main.py
+++ Ecology_Project/main.py
@@ -1,70 +1,30 @@
 #Импорт
 from flask import Flask, render_template, request
-#Код взят из другого проекта
+
 
 app = Flask(__name__)
-
-def result_calculate(size, lights, device):
-    #Переменные для энергозатратности приборов
-    home_coef = 100
-    light_coef = 0.04
-    devices_coef = 5   
-    return size * home_coef + lights * light_coef + device * devices_coef 
 
 #Первая страница
 @app.route('/')
 def index():
     return render_template('index.html')
+
 #Вторая страница
 @app.route('/test')
 def test():
     return render_template('test.html')
 
 #Третья страница
-@app.route('/<size>/<lights>')
-def electronics(size, lights):
-    return render_template(
-                            'electronics.html',                           
-                            size = size, 
-                            lights = lights                           
-                           )
+@app.route('/test', methods=['POST'])
+def process_form():
+    button_enter = request.form.get('button_enter')
+    choise = request.form['choise']
+    if choise == '3':
+        answer = 'Правильно!'
+        return render_template('test.html', answer=answer, button_enter=button_enter)
+    else:
+        answer = 'Не правильно!!'
+        return render_template('test.html', answer=answer, button_enter=button_enter)
 
-#Расчет
-@app.route('/<size>/<lights>/<device>')
-def end(size, lights, device):
-    return render_template('end.html', 
-                            result=result_calculate(int(size),
-                                                    int(lights), 
-                                                    int(device)
-                                                    )
-                        )
-#Форма
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-#Результаты формы
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    #Создай переменные для сбора информации
-    name = request.form['name']
-    email = request.form['email']
-    address = request.form['address']
-    date = request.form['date']
-
-    with open('form.txt', 'a',) as f:
-            f.write(name + ' ')
-            f.write(email + ' ')
-            f.write(address + ' ')
-            f.write(date + '\n')
-
-    # здесь вы можете сохранить данные или отправить их по электронной почте
-    return render_template('form_result.html', 
-                           #Помести переменные
-                           name=name,
-                           email = email,
-                           address = address,
-                           date = date
-                           )
 
 app.run(debug=True)
